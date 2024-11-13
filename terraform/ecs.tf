429c72ff250f3a8e452d15e01b54469535d277a2
nmywrld <111645407+nmywrld@users.noreply.github.com>
2024-11-13 18:02:59 +0800

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -147,7 +147,7 @@ resource "aws_appautoscaling_policy" "frontend_scaling_policy" {
   service_namespace  = aws_appautoscaling_target.frontend_scaling_target.service_namespace
 
   target_tracking_scaling_policy_configuration {
-    target_value       = 50.0
+    target_value       = 20
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
